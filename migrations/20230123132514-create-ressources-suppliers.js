6022de632c8192e67fc89d5c834dcd38de7a32f7
MarcusNCo <levert.marc.a@gmail.com>
2023-01-23 14:31:21 +0100

    migration Marcus

---
+++ migrations/20230123132514-create-ressources-suppliers.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Ressources_Suppliers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_: {
+        type: Sequelize.INTEGER
+      },
+      id_ressources: {
+        type: Sequelize.INTEGER
+      },
+      id_suppliers: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Ressources_Suppliers');
+  }
+};
---
+++ models/measurement_units.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Measurement_Units extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Measurement_Units.init({
+    id: DataTypes.INTEGER,
+    unity: DataTypes.STRING,
+    id_ressources: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Measurement_Units',
+  });
+  return Measurement_Units;
+};
