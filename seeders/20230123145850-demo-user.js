b45f3b8e844a588fb9ec9b779ec25a9b19376706
LegrandRemy <legrandremy19@outlook.fr>
2023-01-23 16:17:52 +0100

    first seed users

---
+++ seeders/20230123145850-demo-user.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          lastName: 'Legrand',
+          firstName: 'Rémy',
+          email: 'example@example.com',
+          phone: '0627662711',
+          adress: '69 Rue de la poupée qui tousse',
+          zipcode: '80260',
+          city: 'Le Puy Sans Fond',
+          hiring_date: new Date(),
+          salary: 1,
+          id_contract_types: 2,
+          id_roles: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  },
+}
