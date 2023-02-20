3f8c0d0ac3f2b1238a867fad504496653c8c0552
LegrandRemy <legrandremy19@outlook.fr>
2023-02-20 09:36:09 +0100

    re test en prod

--- migrations/23-create-users-meetings.js
+++ migrations/23-create-users-meetings.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users_meetings', {
+    await queryInterface.createTable('Users_Meetings', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -34,6 +34,6 @@ module.exports = {
     })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users_meetings')
+    await queryInterface.dropTable('Users_Meetings')
   },
 }
