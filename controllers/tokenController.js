61383759d4edb66cb0ca8d7c40ccb5968266f05e
zokkyen <stephane.swaenepoel@wanadoo.fr>
2023-10-09 11:55:09 +0200

    fix

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -94,7 +94,7 @@ exports.verifyToken = (req, res, next) => {
     });
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_MAIL);
     req.user = decoded;
     next();
   } catch (error) {
